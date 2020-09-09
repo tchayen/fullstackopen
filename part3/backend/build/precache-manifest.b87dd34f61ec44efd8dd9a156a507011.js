7e311c86a09150cfaa24e88586f6474823eb7957
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-09 23:04:00 +0300

    Update app

---
+++ part3/backend/build/precache-manifest.b87dd34f61ec44efd8dd9a156a507011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ab3328048bf91b840267655434a1bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0c333a2b77143ebbe58",
+    "url": "/static/css/main.eb3c9d66.chunk.css"
+  },
+  {
+    "revision": "f7d5ec0e1cc0e231cfb5",
+    "url": "/static/js/2.20684991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20684991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c333a2b77143ebbe58",
+    "url": "/static/js/main.da1e5294.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const app = express();
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 morgan.token("body", (req, res) => JSON.stringify(req.body));
