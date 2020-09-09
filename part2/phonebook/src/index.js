a641be1220f3227ba79fd1e481782f20eb72a26c
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-09 18:12:48 +0300

    Refactor components

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
-  const [filter, setFilter] = useState("");
+const Filter = ({ filter, setFilter }) => {
+  const onFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      filter shown with <input onChange={onFilterChange} />
+    </div>
+  );
+};
+
+const List = ({ persons, filter }) => (
+  <>
+    {persons
+      .filter((person) => person.name.toLocaleLowerCase().match(filter || ""))
+      .map((person, index) => (
+        <p key={`${person.name}-${index}`}>
+          {person.name} {person.phone}
+        </p>
+      ))}
+  </>
+);
+
+const Form = ({ persons, setPersons }) => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -26,10 +43,6 @@ const App = () => {
     setNewNumber("");
   };
 
-  const onFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
   const onNameChange = (event) => {
     setNewName(event.target.value);
   };
@@ -39,33 +52,39 @@ const App = () => {
   };
 
   return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <div>
+          name: <input onChange={onNameChange} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={onNumberChange} value={newNumber} />
+        </div>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [filter, setFilter] = useState("");
+
+  return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input onChange={onFilterChange} />
-      </div>
+      <Filter filter={filter} setFilter={setFilter} />
       <h2>Add a new number</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          <div>
-            name: <input onChange={onNameChange} value={newName} />
-          </div>
-          <div>
-            number: <input onChange={onNumberChange} value={newNumber} />
-          </div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Form persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
-      {persons
-        .filter((person) => person.name.toLocaleLowerCase().match(filter || ""))
-        .map((person, index) => (
-          <p key={`${person.name}-${index}`}>
-            {person.name} {person.phone}
-          </p>
-        ))}
+      <List persons={persons} filter={filter} />
     </div>
   );
 };
