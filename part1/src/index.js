554d887de0d1b77d26dcce608ffca08bbf5347b7
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-09 13:38:58 +0300

    Refactor to use just an array in 1.4

--- part1/src/index.js
+++ part1/src/index.js
@@ -19,36 +19,35 @@ const Part = ({ part, numberOfExercises }) => (
   </p>
 );
 
-const Total = ({ numberOfExercises }) => (
-  <p>Number of exercises {numberOfExercises}</p>
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises{" "}
+    {parts.map((part) => part.exercises).reduce((a, b) => a + b)}
+  </p>
 );
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
-  const parts = [part1, part2, part3];
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total
-        numberOfExercises={parts
-          .map((part) => part.exercises)
-          .reduce((a, b) => a + b)}
-      />
+      <Total parts={parts} />
     </div>
   );
 };
