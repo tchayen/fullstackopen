b1dd540e15cd5c0c3de48e867495214e8290cc07
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-09 13:37:43 +0300

    Refactor in 1.3

--- part1/src/index.js
+++ part1/src/index.js
@@ -25,24 +25,30 @@ const Total = ({ numberOfExercises }) => (
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
+  const parts = [part1, part2, part3];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        parts={[
-          { name: part1, exercises: exercises1 },
-          { name: part2, exercises: exercises2 },
-          { name: part3, exercises: exercises3 },
-        ]}
+      <Content parts={parts} />
+      <Total
+        numberOfExercises={parts
+          .map((part) => part.exercises)
+          .reduce((a, b) => a + b)}
       />
-      <Total numberOfExercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
