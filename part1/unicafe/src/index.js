010ccaed33022b1634f075cf094d7a1d8a3a9c61
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-09 15:47:36 +0300

    Add 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
+const Statistics = ({ good, neutral, bad }) => {
   const weightedSum = good * 1 + neutral * 0 + bad * -1;
   const totalCount = good + neutral + bad;
   const average = weightedSum / totalCount;
 
   return (
-    <div>
-      <h1>Give Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+    <>
       <h2>Statistics</h2>
       <strong>{good}</strong> good opinions
       <br />
@@ -31,7 +23,22 @@ const App = () => {
       <br />
       <strong>{((good / totalCount) * 100 || 0).toFixed(2)}%</strong> were
       positive
-      <br />
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
