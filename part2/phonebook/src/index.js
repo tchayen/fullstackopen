d2b4888782bd15aee3a39b964ee3389de47c6396
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-09 17:59:14 +0300

    Add 2.6

---
+++ part2/phonebook/src/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
+
+  const onChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          name: <input onChange={onChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => (
+        <p key={`${person.name}-${index}`}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
