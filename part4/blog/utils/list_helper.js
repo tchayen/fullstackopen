7abb9f31804f454110647b8129a02f300ba33548
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-10 14:56:21 +0300

    Add 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.map((blog) => blog.likes).reduce((a, b) => a + b, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
