21fed34d7067643d564d0c45e630c4a7bb4b67f1
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-09-10 15:07:52 +0300

    Add 4.6

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -11,8 +11,32 @@ const favoriteBlog = (blogs) => {
   return sorted[0];
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined;
+  }
+
+  const authorToCount = {};
+
+  blogs.forEach((blog) => {
+    if (authorToCount[blog.author]) {
+      authorToCount[blog.author] += 1;
+    } else {
+      authorToCount[blog.author] = 1;
+    }
+  });
+
+  const sorted = Object.entries(authorToCount).sort((a, b) => b[1] - a[1]);
+
+  return {
+    author: sorted[0][0],
+    blogs: sorted[0][1],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
